722d5022dc657f01b579926f5e9922d155a97dca
Brett Hurst <bretthurst@outlook.com>
2018-11-30 19:58:14 -0800

    Implemented touch and mkdir.

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const terminalCommands = require('./terminal-commands');
 
 terminalCommands.ls();
+
+terminalCommands.touch('hello.txt');
+
+terminalCommands.mkdir('./DirA');
+terminalCommands.mkdir('./DirB');
+terminalCommands.mkdir('./DirA/DirC');
--- terminal-commands.js
+++ terminal-commands.js
@@ -10,10 +10,30 @@ module.exports.ls = () => {
   });
 };
 
-module.exports.touch = () => {
+module.exports.touch = (filename) => {
+
+  fs.writeFile(filename, '', (err) => {
+    if (err) throw err;
+    console.log(`Created ${filename}`);
+  });
+
+  //Alternative method using fs.open and fs.close I came across.
+
+  // fs.open(filename, 'w', (err, fd) => {
+  //   if (err) throw err;
+  //   fs.close(fd, err => {
+  //     if (err) throw err;
+  //   });
+  // });
 
 };
 
-module.exports.mkdir = () => {
+module.exports.mkdir = (path) => {
+
+  //Option object setting recursive to true so parent directories will be created.
+  fs.mkdir(path, { recursive: true }, err => {
+    if (err) throw err;
+    console.log(`${path} created.`);
+  });
 
 };
